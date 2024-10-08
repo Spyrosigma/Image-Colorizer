c0e0e95b27782630b5cfbd3eaadcbe63182c84ec
Satyam Namdev <namdev2003satyam@gmail.com>
2024-10-08 20:45:03 +0530

    More changes in the UI

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
 import streamlit as st
 
+st.title("Colorizer")
+st.write("This is a simple web app to colorize black and white images using deep learning models.")
+
 # -------------------- base color ------------------
+
 
 import torch
 from torch import nn
@@ -29,7 +33,6 @@ class BaseColor(nn.Module):
 # ------------------ eccv16 ---------------------
 
 import numpy as np
-
 
 class ECCVGenerator(BaseColor):
     def __init__(self, norm_layer=nn.BatchNorm2d):
@@ -343,64 +346,31 @@ def postprocess_tens(tens_orig_l, out_ab, mode='bilinear'):
 	out_lab_orig = torch.cat((tens_orig_l, out_ab_orig), dim=1)
 	return color.lab2rgb(out_lab_orig.data.cpu().numpy()[0,...].transpose((1,2,0)))
 
-
-# parser = argparse.ArgumentParser()
-# parser.add_argument('-i','--img_path', type=str, default='imgs/test.jpg')
-# # parser.add_argument('--use_gpu', action='store_true', help='whether to use GPU')
-# parser.add_argument('-o','--save_prefix', type=str, default='saved', help='will save into this file with {eccv16.png, siggraph17.png} suffixes')
-# opt = parser.parse_args()
-
 colorizer_eccv16 = eccv16(pretrained=True).eval()
 colorizer_siggraph17 = siggraph17(pretrained=True).eval()
 
-# if(opt.use_gpu):
-# 	colorizer_eccv16.cuda()
-# 	colorizer_siggraph17.cuda()
 
 input_image = st.file_uploader("Upload Image : ", type=["jpg", "jpeg", "png"])
 
 if input_image is not None:
-	img = load_img(input_image)
-	(tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256,256))
+    img = load_img(input_image)
+    (tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256,256))
 
-	img_bw = postprocess_tens(tens_l_orig, torch.cat((0*tens_l_orig,0*tens_l_orig),dim=1))
-	out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs).cpu())
-	out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
+    img_bw = postprocess_tens(tens_l_orig, torch.cat((0*tens_l_orig,0*tens_l_orig),dim=1))
+    out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs).cpu())
+    out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
 
-	plt.imsave(f'eccv16.png{input_image.name}', out_img_eccv16)
-	plt.imsave(f'siggraph17.png{input_image.name}', out_img_siggraph17)
+    plt.imsave(f'eccv16.png{input_image.name}', out_img_eccv16)
+    plt.imsave(f'siggraph17.png{input_image.name}', out_img_siggraph17)
 
-	eccv16_path = f'eccv16_{input_image.name}'
-	siggraph17_path = f'siggraph17_{input_image.name}'
+    eccv16_path = f'eccv16_{input_image.name}'
+    siggraph17_path = f'siggraph17_{input_image.name}'
 
-	plt.imsave(eccv16_path, out_img_eccv16)
-	plt.imsave(siggraph17_path, out_img_siggraph17)
+    plt.imsave(eccv16_path, out_img_eccv16)
+    plt.imsave(siggraph17_path, out_img_siggraph17)
+    
+    st.markdown("### Input and B&W Images:")
+    st.image([img, img_bw], caption=['Original', 'Input'], width=256)
 
-    # Display images using Streamlit
-	st.image([img, img_bw], caption=['Original', 'Input', ], width=256)
-
-    # Optionally, you can also display the saved images
-	st.markdown("## Colorized Images:")
-	st.image([eccv16_path, siggraph17_path], caption=['Output (ECCV 16)', 'Output (SIGGRAPH 17)'], width=256)
-
-	# plt.figure(figsize=(12,8))
-	# plt.subplot(2,2,1)
-	# plt.imshow(img)
-	# plt.title('Original')
-	# plt.axis('off')
-
-	# plt.subplot(2,2,2)
-	# plt.imshow(img_bw)
-	# plt.title('Input')
-	# plt.axis('off')
-
-	# plt.subplot(2,2,3)
-	# plt.imshow(out_img_eccv16)
-	# plt.title('Output (ECCV 16)')
-	# plt.axis('off')
-
-	# plt.subplot(2,2,4)
-	# plt.imshow(out_img_siggraph17)
-	# plt.title('Output (SIGGRAPH 17)')
-	# plt.axis('off')
-	# plt.show()
+    st.markdown("### Colorized Images:")
+    st.image([eccv16_path, siggraph17_path], caption=['Output (ECCV 16)', 'Output (SIGGRAPH 17)'], width=256)
